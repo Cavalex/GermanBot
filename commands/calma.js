e4c867462b93c7b76b09af121495e98964f30ee1
Cavalex <42481044+Cavalex@users.noreply.github.com>
2020-10-24 00:29:37 +0100

    Add files via upload

---
+++ commands/calma.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'calma',
+    description: "this is a lol command!",
+    execute(message, args){ 
+        message.channel.send( 'CALMA O CARALHO MEU! É SEMPRE A MESMA MERDA, NÃO VOU JOGAR MAIS ISTO, QUE MERDA DA JOGO!');
+    }
+}
